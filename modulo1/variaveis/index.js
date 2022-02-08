933a586ab8850c305775e0feb0fed188e2836d88
hooks-Gabriele-Peron <gabrieleperon84@gmail.com>
2022-02-08 00:37:17 -0300

    exercicio variaveis

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,44 @@
+let nome 
+let idade 
+console.log(typeof nome, typeof idade)
+
+/* Deu undefined pois não especifiquei valor da variavel*/
+
+
+let nomeDeUsuario = prompt('Como você se chama?')
+let idadeUsuario = prompt('qauntos anos tem?')
+
+console.log(nomeDeUsuario, idadeUsuario)
+
+//ele imprimiu os informações do usuario
+
+console.log('Olá', nomeDeUsuario, 'você tem', idadeUsuario,'anos.')
+
+
+let timeDoCoracao = prompt('Você é Palmeirense?') 
+let profissao = prompt('você estuda?')
+let ocupacao = prompt('Está trabalhando?')
+
+
+
+console.log('Você é Palmeirense?', timeDoCoracao)
+console.log('você estuda?', profissao)
+console.log('Está trabalhando?', ocupacao)
+
+
+let a = 10
+let b = 25
+//tentar trocar os valores
+
+c = a
+b = c
+a = 25
+console.log(a, b)
+
+
+
+
+
+
+
+

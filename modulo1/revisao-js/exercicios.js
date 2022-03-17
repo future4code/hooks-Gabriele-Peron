1ca4fa315107ded57db0042a5b64a8fb50548e7b
GabrielePeron <gabrieleperon84@gmail.com>
2022-03-17 14:05:12 -0300

    exercicio 11

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -104,24 +104,7 @@ function retornaSegundoMaiorESegundoMenor(array) {
 
 // EXERCÍCIO 11
 function retornaChamadaDeFilme(filme) {
-    //const filme = {
-    //     nome: 'O Diabo Veste Prada',
-    //     ano: 2006,
-    //     diretor: 'David Frankel',
-    //     atores: ['Meryl Streep', 'Anne Hathaway', 'Emily Blunt', 'Stanley Tucci']
-    // }
-    // // const separaAtor = filme.atores.map((item, index)=>{
-    // //     if(index === 0){
-    // //       return  item
-    // //     } else {
-    // //       return  " "+item
-    // //     }
-
-    // // })
-
-
-    // // return `Venha assistir ao filme ${filme.nome}, de ${filme.ano}, dirigido por ${filme.diretor} e estrelado por ${separaAtor}.`
-    // return `Venha assistir ao filme ${filme.nome}, de ${filme.ano}, dirigido por ${filme.diretor} e estrelado por ${filme.atores[0]}, ${filme.atores[1]}, ${filme.atores[2]}, ${filme.atores[3]}.`
+    return `Venha assistir ao filme ${filme.nome}, de ${filme.ano}, dirigido por ${filme.diretor} e estrelado por ${filme.atores[0]}, ${filme.atores[1]}, ${filme.atores[2]}, ${filme.atores[3]}.`
 }
 
 // EXERCÍCIO 12

8f84b8e2b92d7ed0f0ed5ae40d7b602a0c01892d
GabrielePeron <gabrieleperon84@gmail.com>
2022-04-28 20:23:53 -0300

    Listas

--- modulo3/estados-insta4/insta4/src/App.js
+++ modulo3/estados-insta4/insta4/src/App.js
@@ -1,38 +1,49 @@
-import React from "react";
-import styled from "styled-components";
-import Post from "./components/Post/Post";
+import React from 'react';
+import styled from 'styled-components'
+import Post from './components/Post/Post';
 
-const MainContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  align-items: center;
-`;
+ const MainContanier = styled.div`
+   display: flex;
+   justify-content: center;
+   flex-direction: column;
+   align-items: center
+   `
+
 
 class App extends React.Component {
-  render() {
-    return (
-      <MainContainer>
-        <Post
-          nomeUsuario={"paulinha"}
-          fotoUsuario={"https://picsum.photos/50/50"}
-          fotoPost={"https://picsum.photos/200/150"}
-        />
-
-        <Post
-          nomeUsuario={"pedrinho"}
-          fotoUsuario={"https://picsum.photos/50/70"}
-          fotoPost={"https://picsum.photos/200/130"}
-        />
-
-        <Post
-          nomeUsuario={"joão"}
-          fotoUsuario={"https://picsum.photos/50/60"}
-          fotoPost={"https://picsum.photos/200/140"}
-        />
-      </MainContainer>
-    );
+  state={
+    usuario:[
+      {
+        nomeUsuario:'paulinha',
+        fotoUsuario:<img src='https://picsum.photos/50/50'></img>,
+        fotoPost:<img src='https://picsum.photos/200/150'></img>
+      },
+      {
+        nomeUsuario:'Pedro',
+        fotoUsuario:<img src='https://picsum.photos/50/60'></img>,
+        fotoPost:<img src='https://picsum.photos/200/143'></img>
+      },
+      {
+        nomeUsuario:'joão',
+        fotoUsuario:<img src='https://picsum.photos/50/80'></img>,
+        fotoPost:<img src='https://picsum.photos/200/140'></img>
+      }
+    ]
   }
-}
+render() {
+  const listaComponentes = this.state.usuario.map((usuario) =>{
+    return(
+      <p>
+        {usuario.nomeUsuario} - {usuario.fotoUsuario} - {usuario.fotoPost}
+      </p>
+    )
+  })
 
+  return(
+    <MainContanier>
+    {listaComponentes}
+    </MainContanier>
+  )
+}
+}
 export default App;
